b2cac4cc116c9f5b6fe8a832a7c07d79f14658ba
Louis Yang <louis.yang.925@gmail.com>
2017-04-08 00:35:27 -0700

    Neural Network v1.01
    Separate examples from the neural network module code

---
+++ examples.py
@@ -0,0 +1,119 @@
+from neural_network import *
+
+#test cases
+def test0():
+    x = [[1],[2],[3],[4]]
+    y = [[0],[0],[1],[1]]
+    sl = []
+    nn = NeuralNetwork(len(x[0]), len(y[0]), sl)
+    nn.train(x,y,100,0.3)
+    return nn
+
+def test1():
+    x = [[1.0,1.0,1.0], [2.0,2.0,2.0], [3.0,3.0,3.0], [4.0,4.0,4.0]]
+    y = [[1,0], [0,1], [1,1], [0,0]]
+    sl = [5,3] #2 hidden layers
+    nn = NeuralNetwork(len(x[0]), len(y[0]), sl)
+    nn.train(x,y,100,0.3)
+    return nn
+
+def test2():
+    'test the neural network as a logistic function fitting'
+    n = 100
+    a0 = 10.0
+    a1 = 5.0
+    xmax = 10
+    randsample = np.random.rand(n)
+    x = (np.arange(n)/float(n) - 0.5) * xmax
+    #x = a1 * (np.random.rand(n, 1) - 0.5) + a0
+    hofx = gf(x)
+    x = x * a1 + a0
+    y = np.array([1 if randsample[i] < hofx[i] else 0 for i in xrange(n)])
+    #y = np.array([[1] if yi > 0.5 else [0] for yi in y_log])
+    plt.plot(x, y, '+')
+    plt.plot(x, hofx)
+    plt.plot(x, randsample, 'r*')
+    plt.show()
+
+    x = np.transpose([x])
+    y = np.transpose([y])
+
+    sl = [] #no hidden layer
+    runs = 500
+    step = 0.5
+    nn = NeuralNetwork(len(x[0]), len(y[0]), sl)
+    nn.train(x, y, runs, step)
+
+    plt.plot(x, hofx)
+    plt.plot(nn.X_ori, nn.Y_pred, '+')
+    plt.show()
+    
+    return nn
+
+def test3():
+    'test the neural network as a XNOR logic unit'
+    
+    x = np.array([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
+    y = np.array([[1], [0], [0], [1]])
+    
+    sl = [2] #1 hidden layer with 2 units
+    runs = 500
+    step = 0.5
+    nn = NeuralNetwork(len(x[0]), len(y[0]), sl)
+    nn.train(x, y, runs, step)
+    print('t matrices:')
+    print(nn.t)
+    print('t0 vectors:')
+    print(nn.t0)
+    return nn
+
+def test4():
+    'improved test of the neural network as a XNOR logic unit'
+    n = 100
+    xmax = 10
+    x = np.array([[0.0, 0.0]] * n)
+    y = np.array([[0]] * n)
+    for i in xrange(n):
+        a1 = xmax * (np.random.rand() - 0.5)
+        a2 = xmax * (np.random.rand() - 0.5)
+        if a1 * a2 > 0:
+            y[i, 0] = 1
+        else:
+            y[i, 0] = 0
+        x[i, 0] = a1
+        x[i, 1] = a2
+
+    n_p = np.sum(y) #number of positive data points
+
+    x1p = np.array([0.0] * n_p)
+    x2p = np.array([0.0] * n_p)
+
+    x1n = np.array([0.0] * (n - n_p))
+    x2n = np.array([0.0] * (n - n_p))
+    
+    i_p = 0
+    i_n = 0
+    for i in xrange(n):
+        if y[i, 0] == 1:
+            x1p[i_p] = x[i, 0]
+            x2p[i_p] = x[i, 1]
+            i_p += 1
+        else:
+            x1n[i_n] = x[i, 0]
+            x2n[i_n] = x[i, 1]
+            i_n += 1
+
+    plt.plot(x1p, x2p, 'o')
+    plt.plot(x1n, x2n, 'x')
+    plt.show()
+    
+    sl = [2] #1 hidden layer with 2 units
+    runs = 500
+    step = 0.2
+    nn = NeuralNetwork(len(x[0]), len(y[0]), sl)
+    nn.train(x, y, runs, step)
+    print('t matrices:')
+    print(nn.t)
+    print('t0 vectors:')
+    print(nn.t0)
+    return nn
--- neural_network.py
+++ neural_network.py
@@ -24,7 +24,7 @@ def cost_f_i(y, y_pred):
     else:
         return 0.0
 
-cost_f = np.vectorize(cost_f_i)
+cost_f = np.vectorize(cost_f_i) #vectorized cost_f_i(y, y_pred)
 
 class NeuralNetwork(object):
 
@@ -62,7 +62,7 @@ class NeuralNetwork(object):
         #Normalize X:
         self.normal_X()
         
-        #Initialize the t matrix:
+        #Initialize the t matrices and t0 vectors:
         self.init_t()
         
         #Complete the code for training neural network:
@@ -157,126 +157,8 @@ class NeuralNetwork(object):
         for l in xrange(self.n_layers):
             self.t[l] -= step * self.t_grad[l]
             self.t0[l] -= step * self.t0_grad[l]
-        return
-    
-#test cases
-def test0():
-    sl = []
-    x = [[1],[2],[3],[4]]
-    y = [[0],[0],[1],[1]]
-    nn = NeuralNetwork(len(x[0]), len(y[0]), sl)
-    nn.train(x,y,100,0.3)
-    return nn
+        return 
 
-def test1():
-    x = [[1.0,1.0,1.0], [2.0,2.0,2.0], [3.0,3.0,3.0], [4.0,4.0,4.0]]
-    y = [[1,0], [0,1], [1,1], [0,0]]
-    sl = [5,3]
-    nn = NeuralNetwork(len(x[0]), len(y[0]), sl)
-    nn.train(x,y,100,0.3)
-    return nn
-
-def test2():
-    'test the neural network as a logistic function fitting'
-    n = 100
-    a0 = 10.0
-    a1 = 5.0
-    xmax = 10
-    sl = []
-    randsample = np.random.rand(n)
-    x = (np.arange(n)/float(n) - 0.5) * xmax
-    #x = a1 * (np.random.rand(n, 1) - 0.5) + a0
-    hofx = gf(x)
-    x = x * a1 + a0
-    y = np.array([1 if randsample[i] < hofx[i] else 0 for i in xrange(n)])
-    #y = np.array([[1] if yi > 0.5 else [0] for yi in y_log])
-    plt.plot(x, y, '+')
-    plt.plot(x, hofx)
-    plt.plot(x, randsample, 'r*')
-    plt.show()
-
-    x = np.transpose([x])
-    y = np.transpose([y])
-
-    runs = 500
-    step = 0.5
-    nn = NeuralNetwork(len(x[0]), len(y[0]), sl)
-    nn.train(x, y, runs, step)
-
-    plt.plot(x, hofx)
-    plt.plot(nn.X_ori, nn.Y_pred, '+')
-    plt.show()
-    
-    return nn
-
-def test3():
-    'test the neural network as a XNOR logic unit'
-    
-    sl = [2]
-    x = np.array([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
-    y = np.array([[1], [0], [0], [1]])
-    
-    runs = 500
-    step = 0.5
-    nn = NeuralNetwork(len(x[0]), len(y[0]), sl)
-    nn.train(x, y, runs, step)
-    print('t matrices:')
-    print(nn.t)
-    print('t0 vectors:')
-    print(nn.t0)
-    return nn
-
-def test4():
-    'improved test of the neural network as a XNOR logic unit'
-    sl = [2]
-
-    n = 100
-    xmax = 10
-    x = np.array([[0.0, 0.0]] * n)
-    y = np.array([[0]] * n)
-    for i in xrange(n):
-        a1 = xmax * (np.random.rand() - 0.5)
-        a2 = xmax * (np.random.rand() - 0.5)
-        if a1 * a2 > 0:
-            y[i, 0] = 1
-        else:
-            y[i, 0] = 0
-        x[i, 0] = a1
-        x[i, 1] = a2
-
-    n_p = np.sum(y) #number of positive data points
-
-    x1p = np.array([0.0] * n_p)
-    x2p = np.array([0.0] * n_p)
-
-    x1n = np.array([0.0] * (n - n_p))
-    x2n = np.array([0.0] * (n - n_p))
-    
-    i_p = 0
-    i_n = 0
-    for i in xrange(n):
-        if y[i, 0] == 1:
-            x1p[i_p] = x[i, 0]
-            x2p[i_p] = x[i, 1]
-            i_p += 1
-        else:
-            x1n[i_n] = x[i, 0]
-            x2n[i_n] = x[i, 1]
-            i_n += 1
-
-    plt.plot(x1p, x2p, 'o')
-    plt.plot(x1n, x2n, 'x')
-    plt.show()
-    
-    runs = 500
-    step = 0.2
-    nn = NeuralNetwork(len(x[0]), len(y[0]), sl)
-    nn.train(x, y, runs, step)
-    print('t matrices:')
-    print(nn.t)
-    print('t0 vectors:')
-    print(nn.t0)
-    return nn
 
 
 
